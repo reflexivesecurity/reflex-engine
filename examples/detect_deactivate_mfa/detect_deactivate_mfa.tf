71413d00b2bec9a94b39ca8f23f10097bf1a96ad
rjulian <richard@rjulian.net>
2019-11-24 12:40:54 -0800

    Change directory structure to allow for isolated detective measures

--- examples/detect_deactivate_mfa/detect_deactivate_mfa.tf
+++ examples/detect_deactivate_mfa/detect_deactivate_mfa.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "detect_deactivate_mfa" {
-  source = "../modules/cwe_sns_email"
+  source = "../../modules/cwe_sns_email"
   rule_name = "DetectMFADeactivate"
   rule_description = "Rule to check when MFA Devices are Deactivated"
 
