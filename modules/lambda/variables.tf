52e4e32b775d82676749f199a81046d7b19fae38
rjulian <richard@rjulian.net>
2020-02-16 23:24:48 -0800

    Add in variable for lambda module

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -32,4 +32,8 @@ variable "custom_lambda_policy" {
   description = "Lambda policy specific to invoked lambda"
   type        = string
 }
+variable "sns_topic_arn" {
+  description = "Topic arn for deployed notification topic"
+  type        = string
+}
 
