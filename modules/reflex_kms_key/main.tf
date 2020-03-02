7b602a5537754e939756eedb0c2a40131a6e9712
rjulian <richard@rjulian.net>
2020-03-01 20:51:03 -0800

    Add in KMS key module

---
+++ modules/reflex_kms_key/main.tf
@@ -0,0 +1,71 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+resource "aws_kms_key" "reflex_key" {
+  description             = "Reflex KMS Key"
+  deletion_window_in_days = 7
+  policy                  = <<EOF
+{
+    "Version": "2012-10-17",
+    "Id": "key-consolepolicy-3",
+    "Statement": [
+        {
+            "Sid": "Enable IAM User Permissions",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+            },
+            "Action": "kms:*",
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow use of the key",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": [
+                    "events.amazonaws.com",
+                    "lambda.amazonaws.com",
+                    "sqs.amazonaws.com"
+                ]
+            },
+            "Action": [
+                "kms:Encrypt",
+                "kms:Decrypt",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:DescribeKey"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow access through Simple Queue Service (SQS) for all principals in the account that are authorized to use SQS",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Action": [
+                "kms:Encrypt",
+                "kms:Decrypt",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:CreateGrant",
+                "kms:DescribeKey"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "kms:ViaService": "sqs.${data.aws_region.current.name}.amazonaws.com",
+                    "kms:CallerAccount": "${data.aws_caller_identity.current.account_id}"
+                }
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_kms_alias" "reflex_alias" {
+  name          = "alias/ReflexTestKey"
+  target_key_id = "${aws_kms_key.reflex_key.key_id}"
+}
