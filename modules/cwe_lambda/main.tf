87afc99458633fe4f3becf74e6e4794f1fcef6d5
Max Anderson <maxwell.c.anderson@gmail.com>
2020-03-04 10:31:52 -0800

    Refactor module locations, remove unneeded modules

--- modules/cwe_lambda/main.tf
+++ modules/cwe_lambda/main.tf
@@ -1,12 +1,33 @@
 module "cloudwatch_event_rule" {
-  source        = "../event_rule"
+  source        = "modules/event_rule"
   name          = var.rule_name
   description   = var.rule_description
   event_pattern = var.event_pattern
 }
 
+module "event_target" {
+  source          = "modules/event_target"
+  event_rule_name = module.cloudwatch_event_rule.id
+  target_id       = var.target_id
+  target_arn      = module.sqs_queue.arn
+}
+
+module "sqs_queue" {
+  source         = "modules/sqs_queue"
+  queue_name     = var.queue_name
+  delay_seconds  = var.delay_seconds
+  cwe_arn        = module.cloudwatch_event_rule.arn
+  sqs_kms_key_id = var.sqs_kms_key_id
+}
+
+module "lambda_event_source_mapping" {
+  source           = "modules/lambda_event_source_mapping"
+  event_source_arn = module.sqs_queue.arn
+  function_name    = module.lambda_endpoint.arn
+}
+
 module "lambda_endpoint" {
-  source                   = "../lambda"
+  source                   = "modules/lambda"
   function_name            = var.function_name
   source_code_dir          = var.source_code_dir
   handler                  = var.handler
@@ -16,25 +37,3 @@ module "lambda_endpoint" {
   sns_topic_arn            = var.sns_topic_arn
   custom_lambda_policy     = var.custom_lambda_policy
 }
-
-module "sqs_queue" {
-  source         = "../sqs_queue"
-  queue_name     = var.queue_name
-  delay_seconds  = var.delay_seconds
-  cwe_arn        = module.cloudwatch_event_rule.arn
-  sqs_kms_key_id = var.sqs_kms_key_id
-}
-
-module "lambda_event_source_mapping" {
-  source           = "../lambda_event_source_mapping"
-  event_source_arn = module.sqs_queue.arn
-  function_name    = module.lambda_endpoint.arn
-}
-
-module "event_target" {
-  source          = "../event_target"
-  event_rule_name = module.cloudwatch_event_rule.id
-  target_id       = var.target_id
-  target_arn      = module.sqs_queue.arn
-}
-
