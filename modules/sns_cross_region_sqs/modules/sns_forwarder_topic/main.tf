567e99dd264543fe4130200ed09710b979168b75
rjulian <richard@rjulian.net>
2020-04-30 17:15:35 -0700

    Add in topic policy to allow communication from events

--- modules/sns_cross_region_sqs/modules/sns_forwarder_topic/main.tf
+++ modules/sns_cross_region_sqs/modules/sns_forwarder_topic/main.tf
@@ -1,6 +1,34 @@
 resource "aws_sns_topic" "forwarder_topic" {
   name              = var.topic_name
   kms_master_key_id = var.kms_key_id
+  policy            = <<EOF
+{
+  "Version": "2008-10-17",
+  "Id": "__default_policy_ID",
+  "Statement": [
+    {
+      "Sid": "events_service",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "events.amazonaws.com"
+      },
+      "Action": [
+        "SNS:GetTopicAttributes",
+        "SNS:SetTopicAttributes",
+        "SNS:AddPermission",
+        "SNS:RemovePermission",
+        "SNS:DeleteTopic",
+        "SNS:Subscribe",
+        "SNS:ListSubscriptionsByTopic",
+        "SNS:Publish",
+        "SNS:Receive"
+      ],
+      "Resource": "${aws_sns_topic.forwarder_topic.arn}"
+    }
+  ]
+}
+EOF
+
 }
 
 resource "aws_sns_topic_subscription" "cross_region_sqs_subscription" {
@@ -9,4 +37,5 @@ resource "aws_sns_topic_subscription" "cross_region_sqs_subscription" {
   raw_message_delivery = true
   endpoint             = var.central_queue_arn
 }
+
 
