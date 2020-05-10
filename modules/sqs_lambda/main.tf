bd6d42be26f849de54a4877a12a148e065dfbf5b
rjulian <richard@rjulian.net>
2020-05-10 11:13:55 -0700

    Re-order modules to reflect deployment order

--- modules/sqs_lambda/main.tf
+++ modules/sqs_lambda/main.tf
@@ -2,12 +2,6 @@
 * sqs_lambda: module to generate the targeted sqs queue to lambda ingestion of event payloads
 */
 
-resource "aws_cloudwatch_event_target" "cwe_rule_target" {
-  rule      = var.cloudwatch_event_rule_id
-  target_id = var.target_id
-  arn       = module.sqs_queue.arn
-}
-
 resource "aws_sqs_queue" "sqs_dead_letter_queue" {
   name              = "${var.queue_name}-DLQ"
   kms_master_key_id = var.sqs_kms_key_id
@@ -20,6 +14,12 @@ module "sqs_queue" {
   cwe_arn                   = var.cloudwatch_event_rule_arn
   sqs_kms_key_id            = var.sqs_kms_key_id
   sqs_dead_letter_queue_arn = aws_sqs_queue.sqs_dead_letter_queue.arn
+}
+
+resource "aws_cloudwatch_event_target" "cwe_rule_target" {
+  rule      = var.cloudwatch_event_rule_id
+  target_id = var.target_id
+  arn       = module.sqs_queue.arn
 }
 
 module "sqs_queue_policy" {
@@ -36,12 +36,6 @@ module "sqs_dead_letter_queue_policy" {
   sqs_queue_arn             = module.sqs_queue.arn
 }
 
-module "lambda_event_source_mapping" {
-  source           = "./modules/lambda_event_source_mapping"
-  event_source_arn = module.sqs_queue.arn
-  function_name    = module.lambda_endpoint.arn
-}
-
 module "lambda_endpoint" {
   source                   = "./modules/lambda"
   function_name            = var.function_name
@@ -53,4 +47,10 @@ module "lambda_endpoint" {
   sns_topic_arn            = var.sns_topic_arn
   custom_lambda_policy     = var.custom_lambda_policy
   kms_key_id               = var.sqs_kms_key_id
+}
+
+module "lambda_event_source_mapping" {
+  source           = "./modules/lambda_event_source_mapping"
+  event_source_arn = module.sqs_queue.arn
+  function_name    = module.lambda_endpoint.arn
 }
