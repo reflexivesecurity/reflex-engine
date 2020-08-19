8e2f49472073afff94c2c88267c0ff91f2ed57bb
rjulian <richard@rjulian.net>
2020-08-18 17:48:14 -0700

    Change to string boolean in hopes of actually working

--- modules/sns_cross_account_sqs/main.tf
+++ modules/sns_cross_account_sqs/main.tf
@@ -48,7 +48,7 @@ data "aws_iam_policy_document" "sns_topic_policy" {
 resource "aws_sns_topic_subscription" "cross_region_sqs_subscription" {
   topic_arn            = aws_sns_topic.forwarder_topic.arn
   protocol             = "sqs"
-  raw_message_delivery = true
+  raw_message_delivery = "true"
   endpoint             = "arn:aws:sqs:${var.central_region}:${var.parent_account}:${var.central_queue_name}"
 }
 
