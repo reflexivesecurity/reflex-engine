57e294b0ef43577995ab3b7915af64ba89cfc8c6
Max Anderson <maxwell.c.anderson@gmail.com>
2021-03-09 17:50:25 -0800

    Formatting

--- modules/sqs_lambda/main.tf
+++ modules/sqs_lambda/main.tf
@@ -58,7 +58,6 @@ POLICY
 /*
  * SQS Dead Letter Queue
  */
-
 resource "aws_sqs_queue" "sqs_dead_letter_queue" {
   name              = "${var.queue_name}-DLQ"
   kms_master_key_id = var.sqs_kms_key_id
