e137068deb1f854151e493776f68ffc2d9e3093e
Max Anderson <maxwell.c.anderson@gmail.com>
2021-03-09 16:28:56 -0800

    Pull up sqs queue resource

--- modules/sqs_lambda/main.tf
+++ modules/sqs_lambda/main.tf
@@ -7,33 +7,41 @@ resource "aws_sqs_queue" "sqs_dead_letter_queue" {
   kms_master_key_id = var.sqs_kms_key_id
 }
 
-module "sqs_queue" {
-  source                    = "./modules/sqs_queue"
-  queue_name                = var.queue_name
-  delay_seconds             = var.delay_seconds
-  cwe_arn                   = var.cloudwatch_event_rule_arn
-  sqs_kms_key_id            = var.sqs_kms_key_id
-  sqs_dead_letter_queue_arn = aws_sqs_queue.sqs_dead_letter_queue.arn
+/*
+* sqs_queue: Creation of sqs queue for the purposes of passing events to Lambda
+*/
+resource "aws_sqs_queue" "sqs_queue" {
+  name                       = var.queue_name
+  delay_seconds              = var.delay_seconds
+  kms_master_key_id          = var.sqs_kms_key_id
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.sqs_dead_letter_queue.arn
+    maxReceiveCount     = var.max_receive_count
+  })
+  tags = {
+    Reflex = "true"
+  }
 }
 
 resource "aws_cloudwatch_event_target" "cwe_rule_target" {
   rule      = var.cloudwatch_event_rule_id
   target_id = var.target_id
-  arn       = module.sqs_queue.arn
+  arn       = aws_sqs_queue.sqs_queue.arn
 }
 
 module "sqs_queue_policy" {
   source        = "./modules/sqs_queue_policy"
   cwe_id        = var.cloudwatch_event_rule_id
-  sqs_queue_id  = module.sqs_queue.id
-  sqs_queue_arn = module.sqs_queue.arn
+  sqs_queue_id  = aws_sqs_queue.sqs_queue.id
+  sqs_queue_arn = aws_sqs_queue.sqs_queue.arn
 }
 
 module "sqs_dead_letter_queue_policy" {
   source                    = "./modules/sqs_dead_letter_queue_policy"
   sqs_dead_letter_queue_arn = aws_sqs_queue.sqs_dead_letter_queue.arn
   sqs_dead_letter_queue_id  = aws_sqs_queue.sqs_dead_letter_queue.id
-  sqs_queue_arn             = module.sqs_queue.arn
+  sqs_queue_arn             = aws_sqs_queue.sqs_queue.arn
 }
 
 module "lambda_endpoint" {
@@ -43,7 +51,7 @@ module "lambda_endpoint" {
   handler                  = var.handler
   lambda_runtime           = var.lambda_runtime
   environment_variable_map = var.environment_variable_map
-  sqs_queue_arn            = module.sqs_queue.arn
+  sqs_queue_arn            = aws_sqs_queue.sqs_queue.arn
   sns_topic_arn            = var.sns_topic_arn
   kms_key_id               = var.sqs_kms_key_id
   lambda_timeout           = var.lambda_timeout
@@ -57,7 +65,7 @@ module "iam_assume_role" {
 }
 
 resource "aws_lambda_event_source_mapping" "event_source_mapping" {
-  event_source_arn = module.sqs_queue.arn
+  event_source_arn = aws_sqs_queue.sqs_queue.arn
   enabled          = true
   function_name    = module.lambda_endpoint.arn
   batch_size       = 1
